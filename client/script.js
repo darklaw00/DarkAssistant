ca9120cf9a67350909eac7839c6776fa2223e02e
joshua <joshsp87@gmail.com>
2022-12-28 08:23:06 -0600

    update the server url

--- client/script.js
+++ client/script.js
@@ -79,7 +79,7 @@ const handleSubmit = async (e) => {
 
   // fetch date from server -> bot's
 
-  const response = await fetch('http://localhost:5000', {
+  const response = await fetch('https://dark-assistant.onrender.com', {
     method: 'POST',
     headers: {
       'Content-Type': 'application/json'
